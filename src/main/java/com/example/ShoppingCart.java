81a052058da622c9418cb9115eb8d9997b802bf9
Evgeniia Granina <evgeniia.granina@iths.se>
2025-01-29 08:56:57 +0100

    Add test for adding an item to the shopping cart

--- src/main/java/com/example/ShoppingCart.java
+++ src/main/java/com/example/ShoppingCart.java
@@ -3,4 +3,10 @@ package com.example;
 public class ShoppingCart {
 
 
+    public void addItem(String name, int quantity, double price) {
+    }
+
+    public int getItemCount() {
+        return 1;
+    }
 }
--- src/test/java/com/example/ShoppingCartTest.java
+++ src/test/java/com/example/ShoppingCartTest.java
@@ -1,12 +1,23 @@
 package com.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ShoppingCartTest {
 
+    private ShoppingCart cart;
+
+    @BeforeEach
+    public void createShoppingCartObject() {
+        cart = new ShoppingCart();
+    }
+
     @Test
-    void createShoppingCartObject() {
-        ShoppingCart cart = new ShoppingCart();
+    public void testAddItem() {
+        cart.addItem("", 1, 1.0);
+        assertEquals(1, cart.getItemCount());
     }
 
 
