007f89fdc36e780ab90fb9029fbc9f69b4ab8773
Evgeniia Granina <evgeniia.granina@iths.se>
2025-01-29 08:33:47 +0100

    First commit of Shopping Cart

---
+++ src/test/java/com/example/ShoppingCartTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+public class ShoppingCartTest {
+
+    @Test
+    void createShoppingCartObject() {
+        ShoppingCart cart = new ShoppingCart();
+    }
+
+
+}
